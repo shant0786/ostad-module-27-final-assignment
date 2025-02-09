d23cb7e5784031d720e6c8b645b4441380bc74d3
shant0786 <rm.shanto786@gmail.com>
2025-02-09 21:31:21 +0600

    no change

--- client/src/pages/home-page.jsx
+++ client/src/pages/home-page.jsx
@@ -1,6 +1,5 @@
-import Blog from "../components/blog";
 import Hero from "../components/hero";
-
+import Blog from "../components/blog";
 import Layout from "../layout/layout";
 
 function HomePage() {
